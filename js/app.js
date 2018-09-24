f5fc19b5ff1ae852330fd49212b9fa1f9b05446b
Max Tuerpe <maxtuerpe4@gmail.com>
2018-09-24 14:09:38 -0600

    class and instantiate

---
+++ js/app.js
@@ -0,0 +1,9 @@
+class Tamagotchi  {
+    constructor (name){
+        this.name = name;
+        this.hunger = 10;
+        this.sleepiness = 10;
+        this.boredom = 10;
+    }
+}
+const player1 = new Tamagotchi ("klaus");
